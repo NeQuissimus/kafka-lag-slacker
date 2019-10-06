1e0783d9e3c79ed9cb156ae135e39e27765a70b9
Tim Steinbach <tim@nequissimus.com>
2019-10-05 22:27:29 -0400

    2

---
+++ README.md
@@ -0,0 +1,10 @@
+# kafka-lag-slacker
+
+## Environment
+
+- `KAFKA_LAG_EXPORTER`: [Kafka Lag Exporter](https://github.com/lightbend/kafka-lag-exporter) endpoint
+- `SLACK_WEBHOOK`: [Slack webhook](https://api.slack.com/incoming-webhooks) endpoint
+
+## Debug build
+
+`nix-shell -p cargo -p pkgconfig -p openssl -p rustfmt --command "rustfmt ./src/main.rs; SLACK_WEBHOOK='https://localhost/slack' KAFKA_LAG_EXPORTER='http://localhost:8000/lag' cargo run"`
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+extern crate reqwest;
+
+use reqwest::Url;
+use std::env;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let slack = env::var("SLACK_WEBHOOK").expect("Please set SLACK_WEBHOOK");
+    let slack_url = Url::parse(&slack).expect("SLACK_WEBHOOK must be a valid URL");
+    let lag_exporter = env::var("KAFKA_LAG_EXPORTER").expect("Please set KAFKA_LAG_EXPORTER");
+    let lag_exporter_url =
+        Url::parse(&lag_exporter).expect("KAFKA_LAG_EXPORTER must be a valid URL");
+
+    // Threshold
+
+    println!("{}", slack_url);
+    println!("{}", lag_exporter_url);
+
+    let metric = "kafka_consumergroup_group_max_lag";
+    let metric_prefix = format!("{}{{", metric);
+
+    let response = reqwest::get(lag_exporter_url)
+        .and_then(|mut resp| resp.text())
+        .map(|s| {
+            s.lines()
+                .filter(|s| s.starts_with(&metric_prefix))
+                .map(|s| s.trim_start_matches(metric))
+                .map(str::to_owned)
+                .collect::<Vec<String>>()
+        });
+
+    println!("{:#?}", response);
+
+    Ok(())
 }
